caae68aae707cac9c3afa5f17a99c683fbb5a46d
Anuradha-Dabhole <anuradhadabhole5020@gmail.com>
2025-08-01 12:21:34 +0530

    new changes

--- src/practiceselenium/Amazon_Autosuggestion.java
+++ src/practiceselenium/Amazon_Autosuggestion.java
@@ -12,24 +12,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.io.FileHandler;
 
-public class Amazon_Autosuggestion {
+public class Amazon_Autosuggestion extends Object {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
+		Amazon_Autosuggestion a1 = new Amazon_Autosuggestion();
+		System.out.println(a1.getClass());
 		EdgeDriver driver = new EdgeDriver();
 		driver.get("https://www.amazon.in");
 		driver.manage().window().maximize();
 		WebElement e1 = driver.findElement(By.id("twotabsearchtextbox"));
 		e1.sendKeys("shoe");
 		Thread.sleep(2000);
-		List<WebElement> e2=driver.findElements(By.xpath("//div[@class='two-pane-results-container']/div/div"));
-		e2.get(e2.size()-1).click();
-		//How to select particular  autosuggestion
-		
-		
-		TakesScreenshot t1=  driver;
-	File source=	t1.getScreenshotAs(OutputType.FILE);
-	File destination	=new File("C:\\Users\\Dell Latitude 3400\\eclipse-workspace\\Selenium1\\Screenshot\\photo.png" + Math.random()   +"");
-    FileHandler.copy(source, destination);
+		List<WebElement> e2 = driver.findElements(By.xpath("//div[@class='two-pane-results-container']/div/div"));
+		e2.get(e2.size() - 1).click();
+		// How to select particular autosuggestion
+
+		TakesScreenshot t1 = driver;
+		File source = t1.getScreenshotAs(OutputType.FILE);
+		//File destination = new File("C:\\Users\\Dell Latitude 3400\\eclipse-workspace\\Selenium1\\Screenshot\\photo"+ Math.random() + ".png");//give the only screenshot with random number
+		File destination = new File("C:\\Users\\Dell Latitude 3400\\eclipse-workspace\\Selenium1\\Screenshot\\photo"+ new Amazon_Autosuggestion().getClass() +".png");//create object of the child class & write method it will give the package name & class name
+		FileHandler.copy(source, destination);
 	}
 
 }
